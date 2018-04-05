de68dcff21d2e27c48b4fbdd1a7704408dd3cf7f
Ivan Guevara <ivagueba@hotmail.com>
2018-04-04 21:07:14 -0600

    Added new tables and basic API Controller

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Projects]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL,
+)
---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee_Skills]
+(
+	[Employee_Id] INT NOT NULL, 
+    [Skill_Id] INT NOT NULL, 
+	primary key (Employee_Id, Skill_Id),
+    CONSTRAINT [FK_Employee_Skills_ToEmployees] FOREIGN KEY ([Employee_Id]) REFERENCES [Employees]([Id]),
+	CONSTRAINT [FK_Employee_Skills_ToSkills] FOREIGN KEY ([Skill_Id]) REFERENCES [Skills]([Id]) 
+
+)
