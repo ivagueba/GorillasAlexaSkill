1d3ba9979598551241bb271c1922ec67cbe825ac
Ivan Guevara <ivagueba@hotmail.com>
2018-04-09 20:58:58 -0600

    Added SignalRClientMethodsHelper to have all the SignalR clients methods in a single place Added SignalR metho to get list of employees and projects Added small refactors.

---
+++ AlexaSkill/Helper/SignalRClientMethods.cs
@@ -0,0 +1,46 @@
+﻿using AlexaSkillGorillas.BL.Services;
+using Microsoft.AspNet.SignalR;
+
+namespace AlexaSkill.Helper
+{
+    public class SignalRClientMethodsHelper
+    {
+        private readonly IHubContext _hubContext;
+        private readonly EmployeeService _employeeService;
+        private readonly ProjectService _projectService;
+
+        public SignalRClientMethodsHelper()
+        {
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<AlexaHub>();
+            _employeeService = new EmployeeService();
+            _projectService = new ProjectService();
+        }
+
+        public void RefreshEmployeesList()
+        {
+            var employees = _employeeService.GetEmployees();
+            _hubContext.Clients.All.GetEmployeesList(employees);
+        }
+
+        public void RefreshProjectsList()
+        {
+            var projects  = _projectService.GetProjects();
+            _hubContext.Clients.All.GetEmployeesList(projects);
+        }
+
+        public void SubmitForm()
+        {
+            _hubContext.Clients.All.SubmitForm();
+        }
+
+        public void ShowForm(int formNumber)
+        {
+            _hubContext.Clients.All.UpdateFormVisibility(formNumber);
+        }
+
+        public void UpdateFormField(string fieldName, string value)
+        {
+            _hubContext.Clients.All.UpdateFormField(fieldName, value);
+        }
+    }
+}
